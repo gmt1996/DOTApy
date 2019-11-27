372294c3a8ae81851b648325d088d8d64cfaa629
Matteo Giannettoni <51764993+rizla96@users.noreply.github.com>
2019-11-27 12:00:27 +0100

    aggiornamento tabelle

--- tabelle/tabelleota.sql
+++ tabelle/tabelleota.sql
@@ -1,116 +1,145 @@
-CREATE DATABASE  IF NOT EXISTS `ota` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `ota`;
--- MySQL dump 10.13  Distrib 8.0.17, for macos10.14 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1    Database: ota
--- ------------------------------------------------------
--- Server version	8.0.18
+-- Host: 127.0.0.1
+-- Creato il: Nov 26, 2019 alle 18:42
+-- Versione del server: 10.1.40-MariaDB
+-- Versione PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `accomodation`
+-- Database: `ota`
+--
+CREATE DATABASE IF NOT EXISTS `ota` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `ota`;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `accomodation`
 --
 
-DROP TABLE IF EXISTS `accomodation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `accomodation` (
-  `IDhotel` int(11) NOT NULL AUTO_INCREMENT,
-  `NomeHotel` varchar(100) NOT NULL,
-  `indirizzo` varchar(160) NOT NULL,
+  `IDhotel` int(11) NOT NULL,
+  `NomeHotel` varchar(64) NOT NULL,
+  `indirizzo` varchar(128) NOT NULL,
   `pazziPer` varchar(1000) DEFAULT NULL,
   `recensioni` varchar(3000) DEFAULT NULL,
   `motivi` varchar(500) DEFAULT NULL,
-  `url` varchar(2000) DEFAULT NULL,
-  PRIMARY KEY (`IDhotel`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `url` varchar(256) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `accomodationprice`
+-- Struttura della tabella `accomodationprice`
 --
 
-DROP TABLE IF EXISTS `accomodationprice`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `accomodationprice` (
   `NomeHotel` varchar(200) NOT NULL,
-  `PrezzoHotel` varchar(100) NOT NULL,
-  `dataSoggiorno` varchar(100) DEFAULT NULL,
-  `dataRicerca` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `PrezzoHotel` int(11) NOT NULL,
+  `dataSoggiorno` date DEFAULT NULL,
+  `dataRicerca` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `accomodationservice`
+-- Struttura della tabella `accomodationservice`
 --
 
-DROP TABLE IF EXISTS `accomodationservice`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `accomodationservice` (
   `IDHotel` int(11) NOT NULL,
-  `IDServizi` int(11) NOT NULL,
-  PRIMARY KEY (`IDHotel`,`IDServizi`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `IDServizi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `servizi`
+-- Struttura della tabella `servizi`
 --
 
-DROP TABLE IF EXISTS `servizi`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `servizi` (
-  `idservizi` int(11) NOT NULL,
-  `value` varchar(100) NOT NULL,
-  PRIMARY KEY (`idservizi`),
-  UNIQUE KEY `value_UNIQUE` (`value`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `id` int(11) NOT NULL,
+  `servizio` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `urlHotel`
+-- Struttura della tabella `urlhotel`
 --
 
-DROP TABLE IF EXISTS `urlHotel`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `urlHotel` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(2000) NOT NULL,
-  `data` varchar(100) DEFAULT NULL,
-  `citta` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `urlhotel` (
+  `id` int(11) NOT NULL,
+  `url` varchar(2048) NOT NULL,
+  `data` date DEFAULT NULL,
+  `citta` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping events for database 'ota'
+-- Indici per le tabelle scaricate
 --
 
 --
--- Dumping routines for database 'ota'
+-- Indici per le tabelle `accomodation`
 --
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+ALTER TABLE `accomodation`
+  ADD PRIMARY KEY (`IDhotel`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indici per le tabelle `accomodationservice`
+--
+ALTER TABLE `accomodationservice`
+  ADD PRIMARY KEY (`IDHotel`,`IDServizi`);
+
+--
+-- Indici per le tabelle `servizi`
+--
+ALTER TABLE `servizi`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `value_UNIQUE` (`servizio`);
+
+--
+-- Indici per le tabelle `urlhotel`
+--
+ALTER TABLE `urlhotel`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `accomodation`
+--
+ALTER TABLE `accomodation`
+  MODIFY `IDhotel` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `servizi`
+--
+ALTER TABLE `servizi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `urlhotel`
+--
+ALTER TABLE `urlhotel`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-11-12 19:48:46
