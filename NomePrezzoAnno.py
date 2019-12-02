bf2af3fd78186242451078685eb35077ace015a8
Matteo Giannettoni <51764993+rizla96@users.noreply.github.com>
2019-12-02 10:02:28 +0100

    Update NomePrezzoAnno.py

--- NomePrezzoAnno.py
+++ NomePrezzoAnno.py
@@ -52,6 +52,7 @@ try:
             ht = driver.find_element_by_xpath('//*[@id="hotellist_inner"]')
             numHt = ht.find_elements_by_class_name('sr_item')
             print(len(numHt))
+            time.sleep(3)
             for i in range(0, len(numHt)):
                 o = numHt[i].find_element_by_class_name('sr-hotel__name').text
                 s = (o,)
