ec3e8fcac350f3f531745ca3d05c63dafbccfd0c
Matteo Giannettoni <51764993+rizla96@users.noreply.github.com>
2020-02-28 13:13:11 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ nomeCitta="Pisa"
 meseInizio="marzo 2020"
 numeroMesi=6
 python3 estrazioneUrlCalFin.py $nomeCitta $meseInizio $numeroMesi
-python3 estrazioneInfoHotel.py
+python3 estrazioneInfoHotel.py $nomeCitta
+python3 NomePrezzoAnno.py
