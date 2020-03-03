bde05594400349446b4032d12b5d86a9050c386e
Matteo Giannettoni <51764993+rizla96@users.noreply.github.com>
2020-03-03 17:27:01 +0100

    update
    inserito file estrazioneInfoHotelRec.py
    che aggiunge l'estrazione di tutte le recensioni per ogni hotel.
    inserito dump con aggiornamento struttura delle tabelle

--- estrazioneInfoHotelRec.py
+++ estrazioneInfoHotelRec.py
@@ -32,6 +32,105 @@ connection = mysql.connector.connect(host = config['mysqlDB']['host'],
        passwd = config['mysqlDB']['pass'],
        db = config['mysqlDB']['db'])
 time.sleep(3)
+def recen():
+    try:
+        coo = driver.find_element_by_xpath('//*[@id="cookie_warning"]/div[2]/a')
+        coo.click()
+    except WDE:
+        print("Not able to find element")
+    try:
+        coo1 = driver.find_element_by_xpath('//*[@id="cookie_warning"]/div/div/div[2]/button')
+        coo1.click()
+    except WDE:
+        print("Not able to find element")
+    time.sleep(2)
+    allRec = driver.find_element_by_xpath('//*[@id="left"]/div[10]/div[14]/button')
+    allRec.click()
+    cust = driver.find_element_by_xpath('//*[@id="review_sort"]')
+    cust.click()
+    recenti = driver.find_element_by_xpath('//*[@id="review_sort"]/option[2]')
+    recenti.click()
+    time.sleep(2)
+    next = driver.find_element_by_xpath('//*[@id="review_list_page_container"]/div[4]/div/div[1]/div/div[3]/a')
+    while(next):
+        time.sleep(1)
+        listaRec = driver.find_element_by_class_name('review_list')
+        numRec = listaRec.find_elements_by_class_name('review_list_new_item_block')
+        print(len(numRec))
+        #driver.find_element_by_class_name('bicon-aclose').click()
+
+        for i in range(0,len(numRec)):
+            name = numRec[i].find_element_by_class_name('bui-avatar-block__title').text
+            title = numRec[i].find_element_by_class_name('c-review-block__title').text
+            nation = numRec[i].find_element_by_class_name('bui-avatar-block__subtitle').text
+            recens = numRec[i].find_element_by_class_name('c-review')
+            pos = recens.find_elements_by_class_name('c-review__row')
+            score = numRec[i].find_element_by_class_name('bui-review-score__badge').text
+
+            rp = ''
+            rn = ''
+
+            try:
+                if(len(pos)>1):
+                    rp = pos[0].find_element_by_class_name('c-review__body').text
+                    rn = pos[1].find_element_by_class_name('c-review__body').text
+                    trad = 'Mostra traduzione'
+                    comm = 'Il cliente non ha lasciato un commento'
+                    piac = 'Cosa è piaciuto · '
+                    piacn = 'Cosa non è piaciuto · '
+                    if trad in rp:
+                        c = trad.split('Mostra traduzione')
+                        rp = ''.join(c)
+                    if comm in rp:
+                        c = comm.split('Il cliente non ha lasciato un commento')
+                        rn = ''.join(c)
+                    if piac in rp:
+                        c = piac.split('Cosa è piaciuto · ')
+                        rp = ''.join(c)
+                    if piacn in rn:
+                        c = piacn.split('Cosa non è piaciuto · ')
+                        rn = ''.join(c)
+                    print(pos[0].text)
+                    print(pos[1].text)
+                else:
+                    rp = pos[0].find_element_by_class_name('c-review__body').text
+                    comm = 'Il cliente non ha lasciato un commento'
+                    piac = 'Cosa è piaciuto · '
+                    if comm in rp:
+                        c = comm.split('Il cliente non ha lasciato un commento')
+                        rp = ''.join(c)
+                    if piac in rp:
+                        c = piac.split('Cosa è piaciuto ·')
+                        rp = ''.join(c)
+                    print(pos[0].text)
+            except WDE:
+                print('no rec')
+            time.sleep(2)
+            try:
+                cursor = connection.cursor()
+                cursor.execute("SELECT max(IDHotel) from accomodation")
+                risultato = cursor.fetchone()
+                cursor.close()
+                mySql_insert_query = """INSERT INTO accomodationrecensioni (idhotel, nome, titolo, recensionePos, recensioneNeg, score, nazione)
+                                    VALUES
+                              (%s, %s, %s, %s, %s, %s, %s) """
+                cursor = connection.cursor()
+                result = cursor.execute(mySql_insert_query, (risultato[0],name,title,rp,rn,score,nation))
+                connection.commit()
+                print("Record inserted successfully into accomodationrecensioni table")
+                cursor.close()
+
+            except mysql.connector.Error as error:
+                 print("Failed to insert record into accomodationrecensioni table {}".format(error))
+        #driver.find_element_by_tag_name('body').send_keys(Keys.END)
+        #time.sleep(2)
+        try:
+            next = driver.find_element_by_xpath('//*[@id="review_list_page_container"]/div[4]/div/div[1]/div/div[3]/a')
+            next.click()
+            time.sleep(2)
+        except:
+            break
+    print('fine recensioni')
 def entraHotel():
     #main_page = driver.current_window_handle
     numHt = driver.find_elements_by_class_name('sr_item')
@@ -89,16 +188,20 @@ def estrazioneInfoHotel():
         x= l[0]
         hturl = (x,)
     except WDE:
-        print("err recensioni")
+        print("err ")
+    javaScript = "return(booking.env.b_map_center_latitude)"
+    js = driver.execute_script(javaScript)
+    javaScript1 = "return(booking.env.b_map_center_longitude)"
+    js1 = driver.execute_script(javaScript1)
     try:
 
-        mySql_insert_query = """INSERT INTO accomodation (NomeHotel, indirizzo, url)
+        mySql_insert_query = """INSERT INTO accomodation (NomeHotel, indirizzo, url, latitudine, longitudine)
                VALUES
-               (%s, %s, %s) """
+               (%s, %s, %s, %s, %s) """
 
         cursor = connection.cursor()
         global NomeHote
-        result = cursor.execute(mySql_insert_query, (NomeHote[0], indiri[0], hturl[0]))
+        result = cursor.execute(mySql_insert_query, (NomeHote[0], indiri[0], hturl[0], js, js1))
         connection.commit()
         print("Record inserted successfully into accomodation table")
         cursor.close()
@@ -133,34 +236,6 @@ def estrazioneInfoHotel():
     except WDE:
         print("err pazzi per")
 
-    #estrae tutte le recensioni caricate sulla pagina
-    try:
-        recensioni = driver.find_elements_by_class_name('c-review__body')
-        recen = ''
-        for i in range(0,len(recensioni)):
-            q = recensioni[i].text
-            try:
-                cursor = connection.cursor()
-                cursor.execute("SELECT max(IDHotel) from accomodation")
-                risultato = cursor.fetchone()
-                cursor.close()
-
-                mySql_insert_query = """INSERT INTO accomodationrecensioni (idhotel, recensione)
-                       VALUES
-                       (%s, %s) """
-                cursor = connection.cursor()
-                result = cursor.execute(mySql_insert_query, (risultato[0],q))
-                connection.commit()
-                print("Record inserted successfully into accomodationrecensioni table")
-                cursor.close()
-
-
-            except mysql.connector.Error as error:
-                print("Failed to insert record into accomodationrecensioni table {}".format(error))
-            recen = recen + recensioni[i].text + '; '
-    except WDE:
-        print("err recensioni")
-
     #estare i buoni motivi per scegliere la struttura
     try:
         motivi3 = driver.find_elements_by_class_name('oneusp')
@@ -187,7 +262,7 @@ def estrazioneInfoHotel():
                 print("Failed to insert record into accomodationmotivi table {}".format(error))
             mot = mot + motivi3[i].text + '; '
     except WDE:
-        print("err recensioni")
+        print("err motivi")
 
     #estrae tutte le categorie e per ognuna le sue info
     try:
@@ -241,7 +316,10 @@ def estrazioneInfoHotel():
             print("MySQL connection is closed")
     except WDE:
         print("err recensioni")
-
+    try:
+        recen()
+    except WDE:
+        print('errore recensioni')
     driver.close()
     driver.switch_to.window(main_page)
 
